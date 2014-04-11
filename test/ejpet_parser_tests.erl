96405f6edfa1a8a04b43150a0d27004d3a1fe7ac
Nicolas Michel <nicolas.michel.lava@gmail.com>
2014-04-11 11:26:37 +0200

    * Update parser tests.

--- test/ejpet_parser_tests.erl
+++ test/ejpet_parser_tests.erl
@@ -8,17 +8,22 @@
 capture_test_() ->
     Tests = [
              {"(?<var1>42)",
-              {capture,{number,42},"var1"}},
+              {{capture,{{number,42},
+                         <<102, 152,199,206,1>>},"var1"},
+               <<99, 102,152,199,206,1, 49,6,87,2>>}},
              {"(!<var1>string)",
-              {inject, string, "var1"}},
+              {{inject, string, "var1"}, <<105, 71,144,171,7, 49,6,87,2>>}},
              {"(!<var1>boolean)",
-              {inject, boolean, "var1"}},
+              {{inject, boolean, "var1"}, <<105, 30,43,102,1, 49,6,87,2>>}},
              {"(!<var1>number)",
-              {inject, number, "var1"}},
+              {{inject, number, "var1"}, <<105, 194,56,92,7, 49,6,87,2>>}},
              {"(!<var1>regex)",
-              {inject, regex, "var1"}},
+              {{inject, regex, "var1"}, <<105, 200,189,120,0, 49,6,87,2>>}},
              {"{_:[*, (!<var1>regex), *]}",
-              {object,[{pair,any,{list,[{find,{inject,regex,"var1"}}]}}]}}
+              {{object,[{{pair,any,{{list,[{find,{{inject,regex,"var1"}, <<105, 200,189,120,0, 49,6,87,2>>}}]},
+                                   <<108, 105,200,189,120,0,49,6,87,2>>}},
+                         <<112,97, 108,105,200,189,120,0,49,6,87,2>>}]},
+               <<111, 112,97,108,105,200,189,120,0,49,6,87,2>>}}
             ],
     lists:foldl(fun({Pattern, Expected}, Acc) ->
                         {[], AST} = ejpet_parser:parse(ejpet_scanner:tokenize(Pattern, [])),
